493e5ee59c701d1c8b6ac6e9a40ffa750187dd2d
santiago ceballos <santiceballos44@outlook.com>
2023-09-16 00:08:14 -0500

    UI :tada: se realizo el diseño de la pagina web de nitrolix

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+/* variables de globales */
+
+// creacion de año dinamico para la pagina web
+let year = document.querySelector('#year');
+let año = new Date().getFullYear();
+year.textContent = año;
