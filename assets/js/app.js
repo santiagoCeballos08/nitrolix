d21ba7f7cab2f9b9a9a8707759c848a2952f6e97
santiago ceballos <santiceballos44@outlook.com>
2023-09-22 23:04:50 -0500

    UI :lipstick: ajustes de los diseños en la pagina web

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
-/* variables de globales */
-
 // creacion de año dinamico para la pagina web
 let year = document.querySelector('#year');
 let año = new Date().getFullYear();
 year.textContent = año;
+
+/* naveacion head */
+let contenedorLinks = document.querySelector('.contenedor__navegacion_links');
+let btn_menu__toggle = document.querySelector('.btn_menu__toggle');
+
+document.addEventListener('DOMContentLoaded', () => {
+	eventos();
+	function eventos() {
+		btn_menu__toggle.addEventListener('click', () => {
+			if (contenedorLinks.classList.contains('hidden')) {
+				contenedorLinks.classList.remove('hidden');
+				return;
+			}
+
+			contenedorLinks.classList.add('hidden');
+		});
+	}
+});
