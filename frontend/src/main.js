f82ccd384d9f4d5f88cdc4f799113215a7eb8dd5
santiago ceballos <santiceballos44@outlook.com>
2024-11-24 13:38:26 -0500

    feat(migration): :sparkles: creacion backend y front
    se crea las carpetas y las configuraicones de los archivos principales del proyecto y la migracion a nuevas tecnologias

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './assets/css/app.css';
+import './assets/css/animaciones.css';
+// import './assets/js/app.js';
 
 const app = createApp(App);
 app.mount('#app');
