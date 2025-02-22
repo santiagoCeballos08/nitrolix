acea99c38057dd4cada54235a0362a6da83d09cd
santiago ceballos <santiceballos44@outlook.com>
2025-02-22 14:09:08 -0500

    feat(migracion): :sparkles: creacion de componentes

---
+++ frontend/src/components/Home/CalidadProductoHome.vue
@@ -0,0 +1,36 @@
+<template>
+	<!-- sobre el producto -->
+	<section class="hero__contenedor__calidad p-12">
+		<p class="title__mini__seccion__calidad text-center font-semibold pt-5" id="sobre_producto">
+			Sobre el producto
+		</p>
+		<div class="title__seccion__calidad mt-6">
+			<h2 class="titulo__calidad text-center text-6xl">Nitrogeno de alta calidad a tu servicio</h2>
+			<p class="info__calidad text-center mt-5 lg:max-w-6xl lg:pt-4">
+				Más allá del impacto en la mejora del producto, nos diferenciamos por el servicio y calidad humana;
+				dando así ese impulso evolutivo con nuestro asesoramiento
+			</p>
+		</div>
+		<div class="contenedor__calidad__cualidades">
+			<div class="contenedor__card card__1">
+				<div class="icon__card"><span class="text-8xl material-symbols-outlined">handshake</span></div>
+				<div class="titulo__card font-bold text-4xl my-3">Compromiso</div>
+				<div class="info__card">Comprometidos de hacerte llegar tu producto</div>
+			</div>
+			<div class="contenedor__card card__2">
+				<div class="icon__card"><span class="text-8xl material-symbols-outlined">group</span></div>
+				<div class="titulo__card font-bold text-4xl my-3">Respaldo</div>
+				<div class="info__card">Nuestros productos están respaldados por una garantia</div>
+			</div>
+			<div class="contenedor__card card__3">
+				<div class="icon__card"><span class="text-8xl material-symbols-outlined">verified_user</span></div>
+				<div class="titulo__card font-bold text-4xl my-3">Seguridad</div>
+				<div class="info__card">Aseguramos nuestro producto con los estandares empresariales</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/components/core/HeaderComponent.vue
@@ -0,0 +1,76 @@
+<template>
+	<!-- Header -->
+	<header class="header" id="home" data-elemento="hola">
+		<!-- INICIO DE CONTACTOS -->
+		<div class="header__informacion bg-black text-white p-4 w-full flex justify-center content-center">
+			<div class="contenedor__header lg:flex lg:gap-x-8 lg:justify-around">
+				<div class="contenedor__infro flex flex-col lg:flex-row lg:justify-center lg:gap-x-8">
+					<div class="informacion__tel flex justify-center pt-6 gap-x-2 lg:pt-2 text-lg">
+						<span><span class="font-bold color__contact"> Llamanos:</span> +57 302-6465782</span>
+					</div>
+					<div class="informacion__email flex justify-center pt-2 lg:pt-2 gap-x-2 text-lg">
+						<span><span class="font-bold color__contact">Email:</span> Nitrolix01@gmail.com</span>
+					</div>
+				</div>
+				<div class="informacion__redes flex justify-center mt-5 lg:mt-0 lg:pt-0 gap-x-3 lg:items-center">
+					<a
+						href="https://instagram.com/nitrolix01?igshid=YTQwZjQ0NmI0OA=="
+						target="_blanc"
+						class="contenedor__redes red-ig p-2 redes flex justify-center">
+						<i class="fa-brands fa-instagram redes"></i>
+					</a>
+					<a
+						href="https://www.facebook.com/profile.php?id=61552800174349&mibextid=9R9pXO"
+						target="_blanc"
+						class="contenedor__redes red-fb p-2 redes flex justify-center justify-items-center">
+						<i class="fa-brands fa-facebook redes"></i>
+					</a>
+					<a
+						href="https://wa.me/3026465782?text=Quiero mas informacion"
+						class="contenedor__redes p-2 red-wpp redes flex justify-center">
+						<i class="fa-brands fa-whatsapp redes"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- FIN DE CONTACTOS -->
+		<nav class="hero__contenedor__navegacion__principal flex justify-center p-3">
+			<div class="contendor__navegacion-grande flex flex-col lg:flex-row gap-x-8">
+				<div class="navegacion__icono flex items-center lg:mb-0">
+					<picture>
+						<source srcset="./public/image/logo_principal.png" class="icono__img" type="image/png" />
+						<img
+							loading="lazy"
+							src="./public/image/logo_principal.png.png"
+							alt="logo de litrolix"
+							class="icono__img" />
+					</picture>
+
+					<a class="lg:hidden btn_menu__toggle"
+						><span class="material-symbols-outlined btn_menu"> menu </span></a
+					>
+				</div>
+
+				<div
+					class="hidden lg:block mt-7 md:mt-0 contenedor__navegacion_links animate__animated animate__fadeIn">
+					<div class="navegacion__rels__links flex lg:gap-x-8 gap-y-8 items-center lg:flex-row flex-col">
+						<a href="#banner" class="font-semibold">Inicio</a>
+						<a href="#sobre_producto" class="font-semibold">Producto</a>
+						<a href="#contenedorCaracteristicas" class="font-semibold">Característica</a>
+						<a href="#contacto" class="font-semibold">Contacto</a>
+						<a
+							href="https://wa.me/3026465782?text=Quiero mas informacion"
+							target="_blank"
+							class="navegacion__boton__compra__mini lg:ml-10 font-bold">
+							Comprar ahora
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
